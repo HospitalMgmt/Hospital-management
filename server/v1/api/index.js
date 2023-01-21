376cb9df74dc49945da338bba3a738e2f09fc630
HospitalMgmt <hospitalmgmt.platosys@gmail.com>
2023-01-21 09:30:55 +0530

    HM project

--- server/v1/api/index.js
+++ server/v1/api/index.js
@@ -6,10 +6,9 @@ import Admin from "./Admin/admin.router.js";
 import Departments from "./Departments/departments.router.js";
 import Specialist from "./Specialist/specialist.router.js";
 import appSettings from "./App_Settings/app.router.js";
-import paymentSettings from "./Payment_Settings/payment.router.js";
+
 import Appointment from "./Appointment/appointment.router.js";
-import Track from "./OP_Tracking/tracking.router.js";
-import Specialist_day_slot from "./Specialist_day_slot/Specialist_day_slot.router.js";
+
 import DoctorLog from "./DoctorLog/Doctorlog.router.js";
 import Contact_us from "./Contact_us/contact.router.js";
 import Report from "./Report/Report.router.js";
@@ -18,11 +17,8 @@ app.use("/user", User);
 app.use("/admin", Admin);
 app.use("/departements", Departments);
 app.use("/specialist", Specialist);
-app.use("/specialistDaySlot", Specialist_day_slot);
 app.use("/appSettings", appSettings);
-app.use("/paymentSettings", paymentSettings);
 app.use("/appointment", Appointment);
-app.use("/track", Track);
 app.use("/doctorlog", DoctorLog);
 app.use("/contact", Contact_us);
 app.use("/reports", Report);
